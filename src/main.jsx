89dc2e9aaada8eb5320eb4d0780b652a4fe454c2
ziondigitallabs-zdl <ziondigitallabs@gmail.com>
2026-07-06 21:18:13 +0530

    Initial commit: Zion Digital Labs website - upload src/main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { Analytics } from '@vercel/analytics/react'
+import { SpeedInsights } from '@vercel/speed-insights/react'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
+    <Analytics />
+    <SpeedInsights />
   </StrictMode>,
 )
